965e3149bfcf6859c190ff2032788f9db1f28611
RAntonyXavierRaj <32294003+RAntonyXavierRaj@users.noreply.github.com>
2018-01-21 14:03:03 +0000

    Update production for new database

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -87,6 +87,12 @@ DATABASES = {
     }
 }
 
+# add this
+import dj_database_url
+db_from_env = dj_database_url.config()  # Overriding the db as postgreSQL 
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
